002df5b782a411ddc8ff38d765af3be6862997d8
3mro <111469986+3mroSA@users.noreply.github.com>
2026-05-23 19:10:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
-const recieve = "خالو باسم";
+let recieve = "";
+let msg = "";
+window.addEventListener('DOMContentLoaded', () => {
+  const params = new URLSearchParams(window.location.search);
+  const data = params.get('n');
+  const reset = params.has('r');
+
+const message = params.get('m');
+
+  if (reset) {
+    localStorage.removeItem('code');
+    localStorage.removeItem('msg');
+  }
+
+  const stored = localStorage.getItem('code');
+  const storedMsg = localStorage.getItem('msg');
+
+  if (stored) {
+  recieve = decodeURIComponent(atob(stored));
+ }
+else if (data) {
+    const decoded = decodeURIComponent(atob(data));
+    recieve = decoded;
+    localStorage.setItem('code', data);
+}
+else recieve = "اخي العزيز"
+
+   if (storedMsg) {
+msg = decodeURIComponent(atob(storedMsg));
+  }
+  else if (message) {
+      const decoded = decodeURIComponent(atob(message));
+      msg = decoded;
+      localStorage.setItem('msg', message);
+  }
+  else msg = "كل عام و انت بخير"
+  
+ 
+  
+});
+
+const scrollT = function (elementId) {
+  const element = document.getElementById(elementId);
+  element.scrollIntoView({ behavior: 'smooth' });
+};
 
 const pre = document.getElementById('pre');
+
 for(let i=0; i<200; i++){
   const star = document.createElement('div');
   star.className = 'star';
@@ -29,36 +74,43 @@ const moon = document.getElementById('moon');
 const preContainer = document.getElementById('pre');
 const content = document.getElementById('content');
 const arrow = document.getElementById('arrow_down');
+
 moon.addEventListener('click', function() {
-  // preContainer.style.display = 'none';
+
   moon.classList.remove('pulse');
   preContainer.classList.add('slideDown');
+
 setTimeout(() => {
   const audio = new Audio('aud.mp3');
   audio.play();
-  
 }, 250);
 
-  
 arrow.classList.add('fade');
+
 arrow.addEventListener('animationend', function() {
   arrow.classList.remove('fade');
   arrow.classList.add('arrow_down');
-  })
+})
 
-
-  
   preContainer.addEventListener('animationend', function() {
     preContainer.style.display = 'none';
     moon.classList.add('float')
-    document.body.style.overflowY = 'auto';
+    document.body.style.overflowY = 'scroll';
     typewrite(recieve, 'name', 100);
+
     setTimeout(() => {
-  typewrite('عيد مبارك', 'sub', 150);
-}, 1500);
+      typewrite('عيد مبارك', 'sub', 150);
+    }, 1500);
   })
 
   content.style.display = 'block';
+
+  setTimeout(() => {
+    // document.getElementById('fact').style.display = 'block';
+    document.getElementById('message').style.display = 'block';
+    document.getElementById('message').textContent = msg;
+  }, 5000);
+
 }, { once: true });
 
-const hint = document.getElementById('hint');
+document.addEventListener("contextmenu", e => e.preventDefault());
